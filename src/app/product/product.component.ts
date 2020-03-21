b956e739bca99746b101c46a3808cc32139d3086
ihalilsungur <ihalilsungur@gmail.com>
2020-03-22 01:41:41 +0300

    shopping projemizde products category'e göre routing ve filter işlemlerini yaptım.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from "@angular/core";
 import { Product } from "./product";
 import { AlertifyService } from "../services/alertify.service";
-import { ProductService } from './product.service';
+import { ProductService } from "./product.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-product",
   templateUrl: "./product.component.html",
   styleUrls: ["./product.component.css"],
-  providers:[ProductService]
+  providers: [ProductService]
 })
 export class ProductComponent implements OnInit {
   constructor(
     private alertifyService: AlertifyService,
-    private productService:ProductService
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute
   ) {}
   title = "Ürün Listesi";
   filterText = "";
   products: Product[];
   //bu metod component ilk acildigi zaman yuklenir.
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(data=>{
-      this.products =data;
+    this.activatedRoute.params.subscribe(params => {
+      this.productService.getProducts(params["categoryId"]).subscribe(data => {
+        this.products = data;
+      });
     });
   }
   addToCart(product) {
